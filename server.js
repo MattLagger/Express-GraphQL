6a608cf5c19bd9d8e4abe256499efdc1ce461b18
Wendreo Matheus <wendreo.pvh@gmail.com>
2019-01-12 12:02:04 -0400

    Added Morgan and Graphql

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const app = require('express')();
+const logger = require('morgan')("short");
+const expressGraphQL = require('express-graphql');
+
+app.use('/graphql', expressGraphQL({
+    graphiql: true
+}))
+
+app.use(logger)
 
 app.listen(4000, () => console.log('Listening'))
